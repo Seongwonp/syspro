ae4593f6107f93c881362742319ee9328b94d027
ParkSeongWon <swp2013@naver.com>
2024-09-26 02:53:46 +0000

    fix

---
+++ chap3/hw1/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "copy.h"
+#define MAX_LINES 5
+
+char line[MAX_LINES][MAXLINE];
+char temp[MAXLINE];
+
+int main(){
+
+	for(int i=0; i<MAX_LINES; i++){
+		gets(line[i]);
+	}
+
+
+	for(int a=0; a<MAX_LINES-1; a++){
+		for(int b=a+1; b<MAX_LINES;b++){
+			if(strlen(line[a])<strlen(line[b])){
+				copy(line[a],temp);
+				copy(line[b],line[a]);
+				copy(temp, line[a]);
+			}
+		}
+	}
+
+	for(int i=0; i<5; i++)
+		printf("%s\n", line[i]);
+
+
+	return 0;
+}
+
