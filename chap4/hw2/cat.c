8fc2d7c7a2c0ea87bff8855aa9f6a8711f18c2a2
ParkSeongWon <swp2013@naver.com>
2024-10-17 04:04:51 +0000

    hw2 fix

--- chap4/hw2/cat.c
+++ chap4/hw2/cat.c
@@ -1,35 +1,42 @@
 #include <stdio.h>
 #include <string.h>
 #define MAXLINE 80
-int main(int argc, char *argv[]){
-	FILE *fp;
-	int c;
-	int line_number = 1;
-	int add_line_numbers = 0;
 
-	 if (argc > 1 && strcmp(argv[1], "-n") == 0) {
+int main(int argc, char *argv[]){
+    FILE *fp;
+    int c;
+    int line_number = 1;
+    int add_line_numbers = 0;
+    int new_line = 1;
+
+    if (argc > 1 && strcmp(argv[1], "-n") == 0) {
         add_line_numbers = 1;
         argc--;
         argv++;
     }
 
+    if (argc < 2) {
+        fp = stdin;
+    } else {
+        for (int i = 1; i < argc; i++) {
+            fp = fopen(argv[i], "r");
+            if (fp == NULL)
+                continue;
 
-	if(argc<2)
-		fp=stdin;
-	else{
-		for(int i=1; i<argc; i++){
-		fp=fopen(argv[i],"r");
-		if (fp == NULL)
-			continue;
-		}
+            while ((c = getc(fp)) != EOF) {
+                if (new_line && add_line_numbers) {
+                    printf("%6d  ", line_number++);
+                    new_line = 0;
+                }
+                putc(c, stdout);
+                if (c == '\n') {
+                    new_line = 1;
+                }
+            }
 
-	 while ((c = getc(fp)) != EOF) {
-         if (c == '\n' && add_line_numbers) {
-              printf("%d: ", line_number++);
-             }
-          putc(c, stdout);
+            fclose(fp);
         }
-	fclose(fp);
-	return 0;
+    }
+    return 0;
 }
-}
+
