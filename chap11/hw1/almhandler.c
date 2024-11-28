a9c99e8c941c78e058cfc99f0b4a413d77a6067b
박성원 <park-seongwon@bagseong-won-ui-MacBookAir.local>
2024-11-28 15:51:12 +0900

    hw done

--- chap11/hw1/almhandler.c
+++ chap11/hw1/almhandler.c
@@ -3,11 +3,29 @@
 #include <unistd.h>
 #include <signal.h>
 
-int signal(int signo, void(*func)(int));
-void alarmHandler();
+int mysignal(int signo, void(*func)(int)){
+        struct sigaction act, oldact;
+        act.sa_handler = func;
+        sigemptyset(&act.sa_mask);
+        act.sa_flags = 0;
+
+        if(sigaction(signo, &act, &oldact)<0){
+                return -1;
+        }
+        return 0;
+}
+
+void alarmHandler(int signo){
+        printf("Wake up\n");
+        exit(0);
+}
 
 int main(){
-	signal(SIGALRM, alarmHandler);
+	if(mysignal(SIGALRM, alarmHandler)<0){
+		perror("signal error");
+		return 1;
+	}
+
 	alarm(5);
 	short i =0;
 	while(1){
@@ -16,14 +34,5 @@ int main(){
 		printf("%d second\n",i);
 	}
 	printf("end\n");
-}
-
-int signal(int signo, void(*func)(int)){
-	
-}
-
-void alarmHandler(int signo){
-	printf("Wake up\n");
-	exit(0);
 }
 
