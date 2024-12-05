7bbf99ab45ba1f36d047c1f4054bcb1576f68cb1
ParkSeongWon <swp2013@naver.com>
2024-12-05 15:47:10 +0000

    hw3 continue

--- chap12/hw3/main.c
+++ chap12/hw3/main.c
@@ -1,35 +1,57 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+#define MAXLINE 100
 #define READ 0
 #define WRITE 1
-int main(int argc, char* argv[]){
-	char str[1024];
-	char *command1, *command2;
-	int fd[2];
 
-	printf("[shell]");
-	fgets(str,sizeof(str),stdin);
-	str[strlen(str)-1]='\0';
-	if(strchr(str,'|')!=NULL){
-		command1 = strtok(str,"| ");
-		command2 = strtok(NULL, "| ");
-	}
-	
-	pipe(fd);
+int main(int argc, char* argv[]) {
+    int pid1, pid2, fd[2];
+ 	 char str[1024];
+	 char *command1, *command2;
+	if(argc == 1 || (argc == 4 && strcmp(argv[2], "|"))){ 
+    	if (argc == 1) {
+        	printf("[shell] ");
+        	fgets(str, sizeof(str), stdin);
+        	str[strlen(str) - 1] = '\0';
 
-	if(fork()==0){
-		close(fd[READ]);
-		dup2(fd[WRITE],1);
-		close(fd[WRITE]);
-		execlp(command1, command1, NULL);
-		perror("pipe");
+        	if (strchr(str, '|') != NULL) {
+            	command1 = strtok(str, "| ");
+            	command2 = strtok(NULL, "| ");
+        	}
+    	} 
+		else if (argc == 4 && strcmp(argv[2], "|") == 0){
+			 command1 = argv[1];
+             command2 = argv[3];
+     
+    	}
+		pipe(fd);
+
+		if (fork() == 0) {
+    		close(fd[READ]);
+    		dup2(fd[WRITE], STDOUT_FILENO);
+    		close(fd[WRITE]);
+    		execlp(command1, command1, NULL);
+    		perror("pipe");
+    		exit(1);
+			} 
+		else {
+    		close(fd[WRITE]);
+    		dup2(fd[READ], STDIN_FILENO);
+    		close(fd[READ]);
+    		execlp(command2, command2, NULL);
+    		perror("pipe");
+    		exit(1);
+			}
 	}
-	else{
-		close(fd[WRITE]);
-		dup2(fd[READ],0);
-		close(fd[READ]);
-		execlp(command2, command2,NULL);
-		perror("pipe");
-	}
+	else {
+        fprintf(stderr, "Usage: %s <command1> | <command2>\n", argv[0]);
+        exit(1);
+    }
+
+    return 0;
 }
+
