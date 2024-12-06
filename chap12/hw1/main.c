c63a6d4f72bb1a4e9d2b5d6c9f0f247e4a5f3474
박성원 <park-seongwon@bagseong-won-ui-MacBookAir.local>
2024-12-06 12:54:30 +0900

    hw1 done hw3 continue

--- chap12/hw1/main.c
+++ chap12/hw1/main.c
@@ -2,37 +2,46 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <string.h>
 
 #define MAXLINE 100
 
 int main(){
-	int n, pid, fd[2];
-	char line[MAXLINE];
-	int child_pid;
+    int n, pid, fd[2];
+    char line[MAXLINE] = "Hello from PID ";
+    char pid_str[10];
+    int child_pid;
 
-	pipe(fd);
+    pipe(fd);
 
-	//Send
-	if((pid=fork())==0){
-		close(fd[0]);
-		child_pid = getpid();
-		printf("Send: [%d] ",child_pid);
-		dup2(fd[1],1);
-		close(fd[1]);
-		printf("Hello from PID %d\n",child_pid);
-		exit(0);
-	}
-	
-	//Recv
-	else{
-		close(fd[1]);
-		wait(NULL);
-		printf("Recv: [%d] ",getpid());	
-		while((n=read(fd[0],line,MAXLINE))>0){
-			write(STDOUT_FILENO,line,n);
-		}
-		close(fd[0]);
-	}
+    if ((pid = fork()) == 0) {
+        close(fd[0]);
+        child_pid = getpid();
+        printf("Send: [%d] ", child_pid);
 
-	exit(0);
+        sprintf(pid_str, "%d", child_pid);
+        strcat(line, pid_str);
+        strcat(line, "\n");
+	printf("%s", line);
+        dup2(fd[1], STDOUT_FILENO);
+        printf("%s", line);
+
+        close(fd[1]);
+        return 0;
+    }
+    else {
+        close(fd[1]);
+        wait(NULL);
+
+        printf("Recv: [%d] ", getpid());
+        fflush(stdout);
+
+        while ((n = read(fd[0], line, MAXLINE)) > 0) {
+            write(STDOUT_FILENO, line, n);
+        }
+
+        close(fd[0]);
+    }
+
+    return 0;
 }
--- chap12/hw3/main.c
+++ chap12/hw3/main.c
@@ -1,35 +1,57 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/wait.h>
+#define MAXLINE 1024
 #define READ 0
 #define WRITE 1
+
 int main(int argc, char* argv[]){
-	char str[1024];
-	char *command1, *command2;
-	int fd[2];
+     int n,pid1, pid2, fd[2];
+     char str[MAXLINE];
+     char *command1, *command2;
+     if(argc > 0){
+        if(argc == 1){
+                printf("[shell]");
+                fgets(str,sizeof(str),stdin);
+                str[strlen(str)-1]='\0';
+                if(strchr(str,'|')!=NULL){
+                        command1=strtok(str,"| ");
+                        command2= strtok(NULL,"| ");
+                }
+        }
+        else if(argc == 4){
+                command1 = argv[1];
+                command2 = argv[3];
+        }
 
-	printf("[shell]");
-	fgets(str,sizeof(str),stdin);
-	str[strlen(str)-1]='\0';
-	if(strchr(str,'|')!=NULL){
-		command1 = strtok(str,"| ");
-		command2 = strtok(NULL, "| ");
-	}
-	
-	pipe(fd);
+        pipe(fd);
 
-	if(fork()==0){
-		close(fd[READ]);
-		dup2(fd[WRITE],1);
-		close(fd[WRITE]);
-		execlp(command1, command1, NULL);
-		perror("pipe");
-	}
-	else{
-		close(fd[WRITE]);
-		dup2(fd[READ],0);
-		close(fd[READ]);
-		execlp(command2, command2,NULL);
-		perror("pipe");
-	}
+        if((pid1=fork())==0){
+                close(fd[READ]);
+                dup2(fd[WRITE],1);
+                close(fd[WRITE]);
+                execlp(command1, command1, NULL);
+                perror("pipe");
+        }
+
+        if((pid2=fork())==0){
+                close(fd[WRITE]);
+                dup2(fd[READ],0);
+                close(fd[READ]);
+                execlp(command2,command2,NULL);
+                perror("pipe");
+        }
+        close(fd[READ]);
+        close(fd[WRITE]);
+        waitpid(pid1,NULL,0);
+        waitpid(pid2,NULL,0);
+        exit(0);
+     }
+     else{
+        perror("Invalid argument count");
+        exit(1);
+     }
 }
