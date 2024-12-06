07dedd7351061022238e0ebf8ec646a4926ab312
박성원 <park-seongwon@bagseong-won-ui-MacBookAir.local>
2024-12-06 14:59:17 +0900

    hw3 done ./main or ./main ls "|" wc

--- chap12/hw3/main.c
+++ chap12/hw3/main.c
@@ -4,55 +4,57 @@
 #include <string.h>
 #include <ctype.h>
 #include <sys/wait.h>
+
 #define MAXLINE 1024
 #define READ 0
 #define WRITE 1
 
-int main(int argc, char* argv[]){
-     int n,pid1, pid2, fd[2];
-     char str[MAXLINE];
-     char *command1, *command2;
-     if(argc > 0){
-        if(argc == 1){
-                printf("[shell]");
-                fgets(str,sizeof(str),stdin);
-                str[strlen(str)-1]='\0';
-                if(strchr(str,'|')!=NULL){
-                        command1=strtok(str,"| ");
-                        command2= strtok(NULL,"| ");
-                }
-        }
-        else if(argc == 4){
-                command1 = argv[1];
-                command2 = argv[3];
+int main(int argc, char* argv[]) {
+    int pid1, pid2, fd[2];
+    char str[MAXLINE];
+    char *args1[MAXLINE], *args2[MAXLINE];
+    char *command1, *command2;
+    int i, j;
+
+    if (argc > 0) {
+        if (argc == 1) {
+            printf("[shell]");
+            fgets(str, sizeof(str), stdin);
+            str[strlen(str) - 1] = '\0';
+            if (strchr(str, '|') != NULL) {
+                command1 = strtok(str, "| ");
+                command2 = strtok(NULL, "| ");
+            }
+        } else if (argc == 4 && strcmp(argv[2], "|") == 0) {
+            command1 = argv[1];
+            command2 = argv[3];
         }
 
         pipe(fd);
 
-        if((pid1=fork())==0){
-                close(fd[READ]);
-                dup2(fd[WRITE],1);
-                close(fd[WRITE]);
-                execlp(command1, command1, NULL);
-                perror("pipe");
+        if ((pid1 = fork()) == 0) {
+            close(fd[READ]);
+            dup2(fd[WRITE], STDOUT_FILENO);
+            close(fd[WRITE]);
+            execlp(command1, command1, NULL);
+            perror("pipe");
         }
 
-        if((pid2=fork())==0){
-                close(fd[WRITE]);
-                dup2(fd[READ],0);
-                close(fd[READ]);
-                execlp(command2,command2,NULL);
-                perror("pipe");
+        if ((pid2 = fork()) == 0) {
+            close(fd[WRITE]);
+            dup2(fd[READ], STDIN_FILENO);
+            close(fd[READ]);
+            execlp(command2, command2, NULL);
+            perror("pipe");
         }
+
         close(fd[READ]);
         close(fd[WRITE]);
-        waitpid(pid1,NULL,0);
-        waitpid(pid2,NULL,0);
+        waitpid(pid1, NULL, 0);
+        waitpid(pid2, NULL, 0);
         exit(0);
-     }
-     else{
+    } else {
         perror("Invalid argument count");
         exit(1);
-     }
+    }
 }
-
