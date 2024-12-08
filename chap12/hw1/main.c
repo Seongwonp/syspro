ca142a6fbc95173fafefdd52951ad0600d4a19dd
박성원 <park-seongwon@bagseong-won-ui-MacBookAir.local>
2024-12-08 18:53:17 +0900

    hw 1 fixed

--- chap12/hw1/main.c
+++ chap12/hw1/main.c
@@ -7,41 +7,30 @@
 #define MAXLINE 100
 
 int main(){
-    int n, pid, fd[2];
-    char line[MAXLINE] = "Hello from PID ";
-    char pid_str[10];
+    int n, pid, length, fd[2];
+    char line[MAXLINE];
+    char massage[MAXLINE];
     int child_pid;
 
     pipe(fd);
-
     if ((pid = fork()) == 0) {
         close(fd[0]);
         child_pid = getpid();
-        printf("Send: [%d] ", child_pid);
-
-        sprintf(pid_str, "%d", child_pid);
-        strcat(line, pid_str);
-        strcat(line, "\n");
-	printf("%s", line);
-        dup2(fd[1], STDOUT_FILENO);
-        printf("%s", line);
-
+	printf("Send: [%d] ", child_pid);
+	sprintf(massage, "Hello from PID %d\n",child_pid);
+	length = strlen(massage)+1;
+	write(fd[1],massage,length);
+	
+	printf("%s", massage);
         close(fd[1]);
         return 0;
     }
     else {
         close(fd[1]);
         wait(NULL);
-
-        printf("Recv: [%d] ", getpid());
-        fflush(stdout);
-
-        while ((n = read(fd[0], line, MAXLINE)) > 0) {
-            write(STDOUT_FILENO, line, n);
-        }
-
-        close(fd[0]);
+	n=read(fd[0],line,MAXLINE);
+        printf("Recv: [%d] %s\n", getpid(),line);
     }
-
+    exit(0);
     return 0;
 }
