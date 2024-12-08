afc83446df0d72833160b20f50faf7ff4cd58f9e
박성원 <park-seongwon@bagseong-won-ui-MacBookAir.local>
2024-12-08 19:19:00 +0900

    hw2 process cleanup & code refactoring

--- chap12/hw2/main.c
+++ chap12/hw2/main.c
@@ -9,21 +9,20 @@
 int main(int argc, char* argv[]){
 	int n,pid1, pid2, fd[2];
 	char line[MAXLINE];
-
+	char massage[MAXLINE];
 	pipe(fd);
 
 	if((pid1=fork())==0){
 		close(fd[0]);
 		printf("input string: ");
-
-		if(fgets(line,MAXLINE, stdin) != NULL){
-			write(fd[1],line, strlen(line)+1);
+		if(fgets(massage,MAXLINE, stdin) != NULL){
+			write(fd[1],massage, strlen(massage)+1);
 		}
 		close(fd[1]);
 		exit(0);
 	}
-
-	waitpid(pid1, NULL, 0);
+	
+	waitpid(pid1,NULL, 0);
 
 	if((pid2=fork())==0){
 		close(fd[1]);
@@ -31,13 +30,12 @@ int main(int argc, char* argv[]){
 			for(int i=0; i<n; i++){
 				line[i]=toupper((unsigned char)line[i]);
 			}
-			write(STDOUT_FILENO,line,n);
 		}
+		printf("%s",line);
 		close(fd[0]);
 		exit(0);
-	} 
-
+	}	
 		close(fd[0]);
-		close(fd[1]);
-		return 0;
+		close(fd[1]);	 
+		return 0; 
 }
